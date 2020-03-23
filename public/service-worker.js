b27273d8e66254572b8e11722e4f3d0300d37935
kornif <kapnudel007@gmail.com>
2020-03-23 23:17:14 +0800

    changed

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26ef654acfa9e88414acfff68db84128.js"
+  "/precache-manifest.836c5f510d467aa795ce56dba6391355.js"
 );
 
 self.addEventListener('message', (event) => {
